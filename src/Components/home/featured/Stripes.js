b671f770d215b2de8e0e6599cb366076dac3c1c2
alexxuss1996 <alekseymelnych132@gmail.com>
2019-03-11 19:15:37 +0200

    add animated text

--- src/Components/home/featured/Stripes.js
+++ src/Components/home/featured/Stripes.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { easePolyOut } from "d3-ease";
-import { Animate } from "react-move";
+import Animate  from "react-move/Animate";
 
 export default class Stripes extends Component {
   state = {
