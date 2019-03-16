fc51e08a5dc315e49ade672b32e9f276be13fc06
alexxuss1996 <alekseymelnych132@gmail.com>
2019-03-16 19:28:34 +0200

    create misc component

---
+++ src/Components/home/matches/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Tag } from "../../utils/misc";
+
+const MatchesHome = () => {
+  return (
+    <div className="home_matches_wrapper">
+      <div className="container">
+        <Tag bck="#0e1731" size="50px" color="#ffffff">
+          Matches
+        </Tag>
+        BLOCKS
+        <Tag bck="#ffffff" size="22px" color="#0e1731" link={true} linkto="/the_team">
+          See more matches
+        </Tag>
+      </div>
+    </div>
+  );
+};
+
+export default MatchesHome;
